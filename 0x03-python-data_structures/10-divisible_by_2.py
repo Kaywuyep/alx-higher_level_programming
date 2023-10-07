059689bdbf4fd86cf6595eafa87635636499b891
Kaywuyep <wuyepkate@gmail.com>
2023-10-07 04:06:08 -0700

    tasks

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,13 @@
+#!/usr/python3
+
+
+'''function that finds all multiples of 2 in a list'''
+
+
+def divisible_by_2(my_list=[]):
+    result = []
+
+    for num in my_list:
+        result.append(num % 2 == 0)
+
+    return result
