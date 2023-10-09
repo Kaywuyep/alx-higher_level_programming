0afbb0a0f524ccb231fa639307bcd9b965ac5376
Kaywuyep <wuyepkate@gmail.com>
2023-10-09 01:58:04 -0700

    task

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -5,11 +5,7 @@
 
 
 def element_at(my_list, idx):
-    if idx < 0:
+    if idx < 0 or idx >= len(my_list):
         return None
-    list_len = len(my_list) - 1
-    if idx > list_len:
-        return None
-    for i, num in enumerate(my_list):
-        if i == idx:
-            return num
+    else:
+        return my_list[idx]
