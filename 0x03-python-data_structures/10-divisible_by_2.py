c6111af0bf83e8024333ce1b3c04842d8dce7413
Kaywuyep <wuyepkate@gmail.com>
2023-10-09 02:24:58 -0700

    tasks

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -5,11 +5,14 @@
 
 
 def divisible_by_2(my_list=[]):
-    result = []
-
+    if not my_list:
+        return
+    new = []
+    isMultiple = True
+    notMultiple = False
     for i in my_list:
         if i % 2 == 0:
-            result.append(True)
+            new.append(isMultiple)
         else:
-            result.append(False)
-    return result
+            new.append(notMultiple)
+    return new
