a4e486a5cbd0a2c57683121cd51519e6418c5384
Kaywuyep <wuyepkate@gmail.com>
2023-10-23 08:07:07 -0700

    tasks

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -10,9 +10,8 @@ void print_python_float(PyObject *p);
  */
 void print_python_list(PyObject *p)
 {
-	Py_ssize_t size, alloc, i;
+	Py_ssize_t size, i;
 	const char *type;
-	PyListObject *list;
 
 	if (!PyList_Check(p))
 	{
@@ -20,24 +19,22 @@ void print_python_list(PyObject *p)
 		return;
 	}
 
-	list = (PyListObject *)p;
 	size = PyList_Size(p);
-	alloc = list->allocated;
 
 	fflush(stdout);
 
 	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %ld\n", size);
-	printf("[*] Allocated = %ld\n", alloc);
 
 	for (i = 0; i < size; i++)
 	{
-		type = Py_TYPE(PyList_GetItem(p, i))->tp_name;
+		PyObject *item = PyList_GetItem(p, i);
+		type = Py_TYPE(item)->tp_name;
 		printf("Element %ld: %s\n", i, type);
 		if (strcmp(type, "bytes") == 0)
-			print_python_bytes(PyList_GetItem(p, i));
+			print_python_bytes(item);
 		else if (strcmp(type, "float") == 0)
-			print_python_float(PyList_GetItem(p, i));
+			print_python_float(item);
 	}
 }
 
@@ -48,7 +45,6 @@ void print_python_list(PyObject *p)
 void print_python_bytes(PyObject *p)
 {
 	Py_ssize_t size, i;
-	PyBytesObject *bytes;
 
 	if (!PyBytes_Check(p))
 	{
@@ -56,21 +52,20 @@ void print_python_bytes(PyObject *p)
 		return;
 	}
 
-	bytes = (PyBytesObject *)p;
 	size = PyBytes_Size(p);
 
 	fflush(stdout);
 
 	printf("[.] bytes object info\n");
-	printf("  [.] size: %ld\n", size);
-	printf("  [.] trying string: %s\n", bytes->ob_sval);
+	printf("  size: %ld\n", size);
+	printf("  trying string: %s\n", PyBytes_AsString(p));
 
 	size = (size >= 10) ? 10 : size + 1;
 
-	printf("  [.] first %ld bytes: ", size);
+	printf("  first %ld bytes: ", size);
 	for (i = 0; i < size; i++)
 	{
-		printf("%02hhx", bytes->ob_sval[i]);
+		printf("%02hhx", PyBytes_AsString(p)[i]);
 		printf((i == size - 1) ? "\n" : " ");
 	}
 }
@@ -81,18 +76,16 @@ void print_python_bytes(PyObject *p)
  */
 void print_python_float(PyObject *p)
 {
-	double value;
-
 	if (!PyFloat_Check(p))
 	{
 		printf("[ERROR] Invalid Float Object\n");
 		return;
 	}
 
-	value = PyFloat_AsDouble(p);
+	double value = PyFloat_AsDouble(p);
 	fflush(stdout);
 
 	printf("[.] float object info\n");
-	printf("  [.] value: %f\n", value);
+	printf("  value: %f\n", value);
 }
 
