b58711e23c422fe0cdf2230021f7d305bf4aeed1
Kaywuyep <wuyepkate@gmail.com>
2023-10-23 04:43:43 -0700

    tasks

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    """prints the first x elements of a list and only integers"""
+    count = 0
+    try:
+        for i in range(x):
+            value = my_list[i]
+            if isinstance(value, int):
+                print("{:d}".format(value), end="")
+                count += 1
+        print()
+        return count
+    except IndexError:
+        print()
+        return count
