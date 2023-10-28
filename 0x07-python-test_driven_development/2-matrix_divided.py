1f145c925888d3ae78cb2b31b26b1d138205183f
Kaywuyep <wuyepkate@gmail.com>
2023-10-28 07:45:26 -0700

    tasks

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -5,25 +5,42 @@ The 2-matrix_divided module supplies one function, matrix_divided(matrix, div).
 
 
 def matrix_divided(matrix, div):
-    """Divides all elements in the matrix by div"""
-    if type(matrix) is not list:
+    """
+    Divides all elements in the matrix by div.
+
+    Args:
+        matrix (list): A matrix (list of lists) of integers/floats.
+        div (int or float): The number to divide all elements by.
+
+    Returns:
+        list: A new matrix with elements divided by div,
+        rounded to 2 decimal places.
+    """
+    if not isinstance(matrix, list):
         raise TypeError("matrix must be a matrix (list of lists)\
                 of integers/floats")
+
     size = None
-    for l in matrix:
-        if type(l) is not list:
+    for row in matrix:
+        # if type(row) is not list
+        if not isinstance(row, list):
             raise TypeError("matrix must be a matrix (list of lists)\
                     of integers/floats")
+
         if size is None:
-            size = len(l)
-        elif size != len(l):
+            size = len(row)
+        elif size != len(row):
             raise TypeError("Each row of the matrix must have the same size")
-        for i in l:
-            if type(i) is not int and type(i) is not float:
+
+        for element in row:
+            if not isinstance(element, (int, float)):
                 raise TypeError("matrix must be a matrix (list of lists)\
                         of integers/floats")
-    if type(div) is not int and type(div) is not float:
+
+    if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
+
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    return [[round(i / div, 2) for i in l] for l in matrix]
+
+    return [[round(element / div, 2) for element in row] for row in matrix]
