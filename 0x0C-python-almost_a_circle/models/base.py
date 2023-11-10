f2b9ad2efa66a3af48933beb8ab1ccac69240181
Kaywuyep <wuyepkate@gmail.com>
2023-11-10 00:12:39 -0800

    tasks

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""a base class module"""
+
+
+class Base:
+    """a first class """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """initialization"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
