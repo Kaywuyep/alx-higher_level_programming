af50f58840bcb0d4e4ff51d34b6c8374ff1661da
Kaywuyep <wuyepkate@gmail.com>
2023-12-12 07:15:39 -0800

    tasks

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -2,5 +2,11 @@
 -- (database hbtn_0c_0) in your MySQL server.
 -- INSERT INTO table (columns) VALUES (values) -> Insert value to selected columns
 INSERT
-INTO first_table (id, name)
-VALUES (89, 'Holberton School')
+INTO first_table (
+	id,
+	name
+)
+VALUES (
+	89,
+	'Holberton School'
+)
