c64df5c3b16597a045da10427c2046a7d69d811a
Kaywuyep <wuyepkate@gmail.com>
2023-12-12 11:07:46 -0800

    conversion

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -8,6 +8,7 @@
 -- Convert the database to UTF8
 ALTER DATABASE hbtn_0c_0 CHARACTER
 SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE hbtn_0c_0;
 
 -- Convert the table to UTF8
 ALTER TABLE first_table CONVERT TO CHARACTER
