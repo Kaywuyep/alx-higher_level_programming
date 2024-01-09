69c2c9ef12a006284243aaaf12d60014b63dd0a8
Kaywuyep <wuyepkate@gmail.com>
2024-01-09 04:01:45 -0800

    advanced task

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,20 +1,32 @@
 #!/usr/bin/node
 const fs = require('fs');
 
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const fileC = process.argv[4];
+
 if (process.argv.length !== 5) {
   console.error('Usage: concat-files.js <fileA> <fileB> <destination>');
   process.exit(1);
 }
 
-const fileAPath = process.argv[2];
-const fileBPath = process.argv[3];
-const destinationPath = process.argv[4];
+if (
+  !fs.existsSync(fileA) ||
+  !fs.statSync(fileA).isFile() ||
+  !fs.existsSync(fileB) ||
+  !fs.statSync(fileB).isFile() ||
+  fileC === undefined
+) {
+  console.error('Invalid arguments. Please check file paths and ensure destination is provided.');
+  process.exit(1);
+}
 
-const fileAContent = fs.readFileSync(fileAPath, 'utf-8');
-const fileBContent = fs.readFileSync(fileBPath, 'utf-8');
+const fileAContent = fs.readFileSync(fileA, 'utf-8');
+const fileBContent = fs.readFileSync(fileB, 'utf-8');
+const stream = fs.createWriteStream(fileC);
 
-const concatenatedContent = `${fileAContent}${fileBContent}`;
+stream.write(fileAContent);
+stream.write(fileBContent);
+stream.end();
 
-fs.writeFileSync(destinationPath, concatenatedContent);
-
-console.log(`Files ${fileAPath} and ${fileBPath} have been concatenated to ${destinationPath}`);
+console.log(`Files ${fileA} and ${fileB} have been concatenated to ${fileC}`);
