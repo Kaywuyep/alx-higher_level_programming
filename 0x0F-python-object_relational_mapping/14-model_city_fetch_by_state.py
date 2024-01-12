375fa23175e9f73c109d02bdcd28ab1da13f7e6c
Kaywuyep <wuyepkate@gmail.com>
2024-01-12 06:14:09 -0800

    task

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,29 +1,27 @@
 #!/usr/bin/python3
 """
-Script that adds the State object "Louisiana" to the database hbtn_0e_6_usa
+ prints all City objects from the database hbtn_0e_14_usa
 """
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 from model_state import Base, State
+from model_city import Base, City
 from sqlalchemy.orm import sessionmaker
 from sys import argv
 
 
 if __name__ == "__main__":
-    """my function adds a new state object louisiana"""
+    """Connect to the database"""
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         argv[1],
         argv[2],
         argv[3]),
         pool_pre_ping=True)
-
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    new_state = State(name="Louisiana")
-
-    session.add(new_state)
-    session.commit()
-    print(new_state.id)
+    for city in session.query(City).join(State).order_by(City.id).all():
+        print("{}: ({}) {}".format(city.state.name, city.id, city.name))
+    
     session.close()
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" a python file that contains the class definition of a
+City and an instance Base = declarative_base()"""
+from sqlalchemy import Integer, String, Column, ForeignKey
+from model_state import Base, State
+
+
+class City(Base):
+    """
+    a class city that inherits from base
+    """
+    __tablename__ = 'cities'
+    id = Column(
+            Integer,
+            primary_key=True,
+            autoincrement=True,
+            nullable=False)
+    name = Column(
+            String(128),
+            nullable=False)
+    state_id = Column(
+            Integer,
+            ForeignKey('states.id'),
+            nullable=False)
+    state = relationship('State')
