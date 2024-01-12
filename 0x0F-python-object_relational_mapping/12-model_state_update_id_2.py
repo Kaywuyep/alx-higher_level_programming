950bb0be710b30ab3bca20a164fc290f4ac1ac8d
Kaywuyep <wuyepkate@gmail.com>
2024-01-12 05:59:22 -0800

    task

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -10,7 +10,7 @@ from sys import argv
 
 if __name__ == "__main__":
     """connect to the database"""
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         argv[1],
         argv[2],
         argv[3]),
