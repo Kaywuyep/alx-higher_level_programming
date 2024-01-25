47d7b2477d8da517484d4b79feb17493e674f0a6
Kaywuyep <wuyepkate@gmail.com>
2024-01-25 04:41:51 -0800

    task

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script takes a URL, sends request to URL, & displays the size of d body
-curl -sI $1 | grep content-length | awk '{print $2}'
+curl -sI $1 | grep Content-Length | awk '{print $2}'
---
+++ 0x10-python-network_0/2-delete.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#  sends a DELETE request to the URL passed as the first arg & displays d body
+curl -sLX DELETE $1
