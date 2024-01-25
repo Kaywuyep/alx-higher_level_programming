eaaea4a07459e7ecae37edee4f57b45a12fc7b6a
Kaywuyep <wuyepkate@gmail.com>
2024-01-25 05:58:18 -0800

    fix

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # makes a request to 0.0.0.0:5000/catch_m & mk d server respond with a msg
-curl -sL -X PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "You got me!"
+curl -sL -X PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin: HolbertonSchool"
