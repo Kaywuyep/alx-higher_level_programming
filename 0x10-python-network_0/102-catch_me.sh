21d98c26479e37b2d40634c17fcbdc2ac44ae4f6
Kaywuyep <wuyepkate@gmail.com>
2024-01-25 05:51:10 -0800

    fix

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # makes a request to 0.0.0.0:5000/catch_m & mk d server respond with a msg
-curl -s -X POST 0.0.0.0:5000/catch_me -d "user_id=98" -w "You got me!\n" -o /dev/null
+curl -sL -X PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "You got me!"
