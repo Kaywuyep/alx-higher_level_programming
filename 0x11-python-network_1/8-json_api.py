154f2a7b2cdf602ef8ea040c930d6a2dcf0c9619
Kaywuyep <wuyepkate@gmail.com>
2024-01-28 01:52:35 -0800

    fix

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -18,5 +18,5 @@ if __name__ == "__main__":
             print('[{}] {}'.format(req.json()['id'], req.json()['name']))
         else:
             print("No result")
-    except:
+    except ValueError:
         print("Not a valid JSON")
