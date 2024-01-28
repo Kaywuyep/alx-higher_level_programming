b27c775a5eb243aeab7fdf4e6706d8de39d49e47
Kaywuyep <wuyepkate@gmail.com>
2024-01-28 01:32:05 -0800

    fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,7 +4,7 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('http://alx-intranet.hbtn.io/status')\
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status')\
             as response:
         html = response.read()
     print('Body response:')
