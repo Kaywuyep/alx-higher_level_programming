3c6c2b1c43d3a755d61b7899c6a7c67df7ca5080
Kaywuyep <wuyepkate@gmail.com>
2024-01-28 02:05:41 -0800

    fix

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -12,7 +12,6 @@ if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
     response = requests.get(url)
     for i in response.json()[:10]:
-        SHA = i.get('SHA') # SHA- typically refers to a Secure Hash Algorithm
+        SHA = i.get('SHA')  # SHA- typically refers to a Secure Hash Algorithm
         author_name = i.get('commit').get('author').get('name')
         print('{}: {}'.format(SHA, author_name))
-
