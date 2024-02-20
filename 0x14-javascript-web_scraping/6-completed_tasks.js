14d20787aa3845f00522dca00b2d2126fd9aa3a5
Kaywuyep <wuyepkate@gmail.com>
2024-02-19 23:45:32 -0800

    task

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+const object = {};
+request(url, function (err, resp, body) {
+  if (resp.statusCode === 200) {
+    for (const task of JSON.parse(body)) {
+      if (task.completed === true) {
+        if (object[task.userId]) {
+          object[task.userId]++;
+        } else {
+          object[task.userId] = 1;
+        }
+      }
+    }
+  } else {
+    console.log(err);
+  }
+  console.log(object);
+});
---
+++ 0x14-javascript-web_scraping/README.md
@@ -0,0 +1,1 @@
+this a simple practice of web scrapping
